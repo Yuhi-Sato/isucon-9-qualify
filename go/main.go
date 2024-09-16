6146db134e9e00a43941be16e855a5f7e38ea7ab
Yuhi-Sato <91863685+Yuhi-Sato@users.noreply.github.com>
2024-09-16 15:22:44 +0900

    category

--- go/main.go
+++ go/main.go
@@ -2295,15 +2295,9 @@ func getSettings(w http.ResponseWriter, r *http.Request) {
 
 	ress.PaymentServiceURL = getPaymentServiceURL()
 
-	categories := []Category{}
-
-	err := dbx.Select(&categories, "SELECT * FROM `categories`")
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
+	for _, c := range categoryCache {
+		ress.Categories = append(ress.Categories, *c)
 	}
-	ress.Categories = categories
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 	json.NewEncoder(w).Encode(ress)
